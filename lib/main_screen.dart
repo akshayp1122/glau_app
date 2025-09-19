b210e8d0fcd03ae3bd2bff0cd43462d542f84391
akshay <ikaw81861@gmail.com>
2025-09-19 19:38:13 +0530

    bottom navigation bar added

---
+++ lib/main_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = const [
+    HomePage(),
+    Center(child: Text("List Page", style: TextStyle(color: Colors.white))),
+    Center(child: Text("Country Page", style: TextStyle(color: Colors.white))),
+    Center(child: Text("Settings Page", style: TextStyle(color: Colors.white))),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true, 
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF0F0F0F), Color(0xFF1C1C1C)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: _pages[_currentIndex],
+      ),
+
+      
+      bottomNavigationBar: Container(
+        margin: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.85),
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.amber.withOpacity(0.4),
+              blurRadius: 10,
+              offset: const Offset(0, 5),
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(30),
+          child: BottomNavigationBar(
+            backgroundColor: Colors.transparent, 
+            elevation: 0, 
+            currentIndex: _currentIndex,
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: Colors.amber,
+            unselectedItemColor: Colors.white70,
+            showUnselectedLabels: true,
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            items: [
+              _buildNavItem(Icons.home, "Home", 0),
+              _buildNavItem(Icons.list, "List", 1),
+              _buildNavItem(Icons.flag, "Country", 2),
+              _buildNavItem(Icons.settings, "Settings", 3),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  BottomNavigationBarItem _buildNavItem(IconData icon, String label, int index) {
+    bool isSelected = _currentIndex == index;
+
+    return BottomNavigationBarItem(
+      icon: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+        child: Transform.scale(
+          scale: isSelected ? 1.3 : 1.0,
+          child: Icon(
+            icon,
+            color: isSelected ? Colors.amber : Colors.white70,
+          ),
+        ),
+      ),
+      label: label,
+    );
+  }
+}
